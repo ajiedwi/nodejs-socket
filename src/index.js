47d384c92ac1f47f53f639940217df2e5d477010
Ajie Dwihastadi <ajiedwi4798@gmail.com>
2023-04-03 22:26:24 +0700

    update router and remove view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-const config = require('../config');
-const app = config.app
-const http = config.http
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/view/index.html');
-});
-
-
-http.listen(3000, function() {
-   console.log('listening on port :3000');
-});
+require('./router')
